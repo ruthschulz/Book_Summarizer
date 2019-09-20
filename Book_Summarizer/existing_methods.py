3a86c5aabe01c058fd68d44c7f57290c9072907e
Ruth Schulz <ruthjschulz@gmail.com>
2019-09-19 18:03:40 -0700

    minor fixes

--- Book_Summarizer/existing_methods.py
+++ Book_Summarizer/existing_methods.py
@@ -31,7 +31,7 @@ for s in range(4):
       text_filename = str(pg_index) + ".txt"
       set_of_documents[s].append(text_filename)
 
-if not os.path.exists('../data/extractive_summaries')
+if not os.path.exists('../data/extractive_summaries'):
   os.makedirs('../data/extractive_summaries')
 
 for t in range(4):
@@ -43,32 +43,31 @@ for t in range(4):
       parser_b = PlaintextParser.from_file('../data/books/' + d, Tokenizer("english"))
       parser_s = PlaintextParser.from_file('../data/summaries/' + d, Tokenizer("english"))
       summary_0 = parser_s.document.sentences
-      for t in [0]:
-        if t==0:
-          summarizer = LuhnSummarizer()
-        elif t==1:
-          summarizer = TextRankSummarizer()
-        elif t==2:
-          summarizer = LsaSummarizer()
-        elif t==3:
-          summarizer = LexRankSummarizer()
-        summary = summarizer(parser_b.document,size)
-        file = '../data/extractive_summaries/' + str(t) + d
-        with open(file,'w') as f:
-          for sentence in summary:
-            f.write(str(sentence) + '\n')
-        rouge_n_score = rouge_n(summary_0,summary)
-        rouge_l_sentence_score = rouge_l_sentence_level(summary_0,summary)
-        rouge_l_summary_score = rouge_l_summary_level(summary_0,summary)
-        model1 = TfDocumentModel(str(summary_0), Tokenizer("en"))
-        model2 = TfDocumentModel(str(summary), Tokenizer("en"))
-        cosine_sim_score = cosine_similarity(model1, model2)
-        scores[s].append([rouge_n_score,rouge_l_sentence_score,rouge_l_summary_score,cosine_sim_score])
-        print([rouge_n_score,rouge_l_sentence_score,rouge_l_summary_score,cosine_sim_score])
-    with open('extractive_summary_scores_' + str(t) + '_size' + str(size) + '.csv', 'w') as csvFile:
-      writer = csv.writer(csvFile)
-      writer.writerows(scores[s])
-    csvFile.close()
+      if t==0:
+        summarizer = LuhnSummarizer()
+      elif t==1:
+        summarizer = TextRankSummarizer()
+      elif t==2:
+        summarizer = LsaSummarizer()
+      elif t==3:
+        summarizer = LexRankSummarizer()
+      summary = summarizer(parser_b.document,size)
+      file = '../data/extractive_summaries/' + str(t) + d
+      with open(file,'w') as f:
+        for sentence in summary:
+          f.write(str(sentence) + '\n')
+      rouge_n_score = rouge_n(summary_0,summary)
+      rouge_l_sentence_score = rouge_l_sentence_level(summary_0,summary)
+      rouge_l_summary_score = rouge_l_summary_level(summary_0,summary)
+      model1 = TfDocumentModel(str(summary_0), Tokenizer("en"))
+      model2 = TfDocumentModel(str(summary), Tokenizer("en"))
+      cosine_sim_score = cosine_similarity(model1, model2)
+      scores[s].append([rouge_n_score,rouge_l_sentence_score,rouge_l_summary_score,cosine_sim_score])
+      print([rouge_n_score,rouge_l_sentence_score,rouge_l_summary_score,cosine_sim_score])
+  with open('extractive_summary_scores_' + str(t) + '_size' + str(size) + '.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(scores[s])
+  csvFile.close()
 
 
 
