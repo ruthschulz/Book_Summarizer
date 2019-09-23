ca8d946fbf6e5b4ea03c751be269ae244e31ef9d
Ruth Schulz <ruthjschulz@gmail.com>
2019-09-23 14:19:49 -0700

    analysis of summaries and testing functions

--- Book_Summarizer/extractive_summarizer.py
+++ Book_Summarizer/extractive_summarizer.py
@@ -9,6 +9,9 @@ from sumy.summarizers.lex_rank import LexRankSummarizer
 from sumy.summarizers.luhn import LuhnSummarizer
 from sumy.summarizers.lsa import LsaSummarizer
 from sumy.summarizers.text_rank import TextRankSummarizer
+from sumy.models import TfDocumentModel
+from sumy.evaluation import cosine_similarity
+from sumy.evaluation import rouge_n, rouge_l_sentence_level, rouge_l_summary_level
 import pathlib
 import pandas as pd
 
@@ -53,12 +56,50 @@ def divide_book_into_chapters(book_id):
     book.close()
 
 
-# calls analyze_summary for each of the summaries to analyze
-# def analyze_summaries():
+# analyze_summary(book_id)
+#
+# analyze the summary using ROUGE scores (n, l_sentence, l_summary) and cosine similarity
+def analyze_summary(book_id):
+    combined_filename = '../data/combined_summaries/' + \
+        str(book_id) + '.txt'
+    summary_filename = '../data/summaries/' + \
+        str(book_id) + '.txt'
+    parser_s = PlaintextParser.from_file(summary_filename, Tokenizer("english"))
+    parser_e = PlaintextParser.from_file(summary_filename, Tokenizer("english"))
+    summary_s = parser_s.document.sentences
+    summary_e = parser_e.document.sentences
+    rouge_n_score = rouge_n(summary_s, summary_e)
+    rouge_l_sentence_score = rouge_l_sentence_level(summary_s, summary_e)
+    rouge_l_summary_score = rouge_l_summary_level(summary_s, summary_e)
+    model1 = TfDocumentModel(str(summary_s), Tokenizer("en"))
+    model2 = TfDocumentModel(str(summary_e), Tokenizer("en"))
+    cosine_sim_score = cosine_similarity(model1, model2)
+    return [rouge_n_score, rouge_l_sentence_score,
+        rouge_l_summary_score, cosine_sim_score]
+    
 
 
-# analyze the summary using ROUGE and cosine similarity
-# def analyze_summary():
+# combine_extractive_summaries(book_id)
+#
+# combines the extractive summaries into a single text file to be analyzed
+def combine_extractive_summaries(book_id):
+    chapter = 0
+    chapter_filename = '../data/extractive_summaries/' + \
+        str(book_id) + '-' + str(chapter) + '.txt'
+    combined_filename = '../data/combined_summaries/' + \
+        str(book_id) + '.txt'
+    path = pathlib.Path(chapter_filename)
+    combined_summary = open(combined_filename, 'w')
+    while path.exists():
+        summary = open(chapter_filename, 'r')
+        for s in summary:
+            combined_summary.write(s)
+        summary.close()
+        chapter += 1
+        chapter_filename = '../data/extractive_summaries/' + \
+            str(book_id) + '-' + str(chapter) + '.txt'
+        path = pathlib.Path(chapter_filename)
+    combined_summary.close()
 
 
 # create_extractive_summary_chapter(book_id, chapter, summarizer, size)
@@ -99,22 +140,75 @@ def create_extractive_summary_book(book_id, summarizer, size):
         path = pathlib.Path(chapter_filename)
 
 
-# for all books listed in data_stats.csv
-# divide into chapters and save in book_chapters folder
-df = pd.read_csv("data_stats.csv", sep=',', header=None)
-for index, row in df.iterrows():
-    pg_index = row[1]
-    divide_book_into_chapters(pg_index)
+
 
 # for all books listed in data_stats.csv
-# create extractive summary using sumy standard summarizer
-# (Luhn, TextRank, Lsa, LexRank)
-# summarizer = TextRankSummarizer()
-# summarizer = LsaSummarizer()
-# summarizer = LexRankSummarizer()
-df = pd.read_csv("data_stats.csv", sep=',', header=None)
-summarizer = LuhnSummarizer()
-for index, row in df.iterrows():
-    pg_index = row[1]
-    print(pg_index)
-    create_extractive_summary_book(pg_index, summarizer, 3)
+# divide into chapters and save in book_chapters folder
+def test_divide_into_chapters():
+    df = pd.read_csv("data_stats.csv", sep=',', header=None)
+    for index, row in df.iterrows():
+        pg_index = row[1]
+        divide_book_into_chapters(pg_index)
+
+
+# for all books listed in data_stats.csv
+# create extractive summary using sumy standard summarizer Luhn
+def test_create_luhn_extractive_summaries():
+    df = pd.read_csv("data_stats.csv", sep=',', header=None)
+    summarizer = LuhnSummarizer()
+    for index, row in df.iterrows():
+        pg_index = row[1]
+        print(pg_index)
+        create_extractive_summary_book(pg_index, summarizer, 3)
+
+# for all books listed in data_stats.csv
+# create extractive summary using sumy standard summarizer LSA
+def test_create_lsa_extractive_summaries():
+    df = pd.read_csv("data_stats.csv", sep=',', header=None)
+    summarizer = LsaSummarizer()
+    for index, row in df.iterrows():
+        pg_index = row[1]
+        print(pg_index)
+        create_extractive_summary_book(pg_index, summarizer, 3)
+
+# for all books listed in data_stats.csv
+# create extractive summary using sumy standard summarizer TextRank
+def test_create_tr_extractive_summaries():
+    df = pd.read_csv("data_stats.csv", sep=',', header=None)
+    summarizer = TextRankSummarizer()
+    for index, row in df.iterrows():
+        pg_index = row[1]
+        print(pg_index)
+        create_extractive_summary_book(pg_index, summarizer, 3)
+
+# for all books listed in data_stats.csv
+# create extractive summary using sumy standard summarizer LexRank
+def test_create_lr_extractive_summaries():
+    df = pd.read_csv("data_stats.csv", sep=',', header=None)
+    summarizer = LexRankSummarizer()
+    for index, row in df.iterrows():
+        pg_index = row[1]
+        print(pg_index)
+        create_extractive_summary_book(pg_index, summarizer, 3)
+
+def test_combine_extractive_summaries():
+    df = pd.read_csv("data_stats.csv", sep=',', header=None)
+    summarizer = LsaSummarizer()
+    for index, row in df.iterrows():
+        pg_index = row[1]
+        print(pg_index)
+        combine_extractive_summaries(pg_index)
+
+
+def test_analyze_summary():
+    df = pd.read_csv("data_stats.csv", sep=',', header=None)
+    summarizer = LsaSummarizer()
+    for index, row in df.iterrows():
+        pg_index = row[1]
+        print(pg_index)
+        print(analyze_summary(pg_index))
+
+
+def test_analyze_summary1(book_id):
+    print(analyze_summary(book_id))
+
